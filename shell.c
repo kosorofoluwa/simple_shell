994599f274a7fc236aa35a4d87d83848d9349691
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-09-22 14:21:59 -0700

    getline function

--- shell.c
+++ shell.c
@@ -22,4 +22,20 @@ void sigintHandler(int sig)
  * Reads a line of input from stdin and stores it in the 'lineptr' variable
  * Dynamically allocates memory for 'lineptr' to accommodate the input line
  */
+void _mygetline(void)
+{
+	size_t n = 0;
+	ssize_t nread;
+
+	nread = getline(&lineptr, &n, stdin);
+	if (nread == -1)
+	{
+		free(lineptr);
+		lineptr = NULL;
+		exit(0);
+	}
+
+	lineptr[nread - 1] = '\0';
+}
+
 
