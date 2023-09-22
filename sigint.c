a402c344664f403a8adb80a5c824a32b498d55fa
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-09-22 14:13:42 -0700

    sigint handler

---
+++ sigint.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+
+/**
+ * sigintHandler - Handles the SIGINT signal (Ctrl+C)
+ * @sig: The signal number (not used in the function)
+ *
+ * Frees the dynamically allocated memory for 'lineptr' and
+ * terminates the program.
+ */
+
