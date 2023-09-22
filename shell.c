9925b2fb8d20efb469bf7fa84f82a9bb97b23f2d
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-09-22 14:27:44 -0700

    prints enviroment

--- shell.c
+++ shell.c
@@ -161,3 +161,28 @@ char *_which(char **env, char *command)
 	}
 	return (NULL);
 }
+
+/**
+ * print_env - Print the current environment variables
+ * @environ: Environment variables
+ **/
+void print_env(char **environ)
+{
+	int i = 0, j = 0;
+	char *var = NULL;
+
+	while (environ[i] != NULL)
+	{
+		var = environ[i];
+		j = 0;
+
+		while (var[j] != '\0')
+		{
+			_putchar(var[j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
