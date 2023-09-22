9c75c046d0512bc8ae4559e0379272446e0f34e9
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-09-22 14:18:26 -0700

    sig int function

--- shell.c
+++ shell.c
@@ -7,4 +7,11 @@
  * Frees the dynamically allocated memory for 'lineptr' and
  * terminates the program.
  */
-
+void sigintHandler(int sig)
+{
+	(void) sig;
+	free(lineptr),
+	lineptr = NULL;
+	_putchar('\n');
+	exit(0);
+}
