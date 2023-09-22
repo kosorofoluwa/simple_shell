eee1147a29486dc3df455155a8e9a0081f2246b0
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-09-22 14:20:28 -0700

    getline comments

--- shell.c
+++ shell.c
@@ -15,3 +15,11 @@ void sigintHandler(int sig)
 	_putchar('\n');
 	exit(0);
 }
+
+/**
+ * _mygetline - Reads a line of input from stdin
+ *
+ * Reads a line of input from stdin and stores it in the 'lineptr' variable
+ * Dynamically allocates memory for 'lineptr' to accommodate the input line
+ */
+
